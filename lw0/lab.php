e67e2881e2ed7896c45f737558fc28e1132af8f6
Timur Ushkov <uskovtimur@gmail.com>
2022-10-11 00:05:07 +0300

    fix(lw0): убрал ошибки

---
+++ lw0/lab.php
@@ -0,0 +1,13 @@
+<?php
+function calculator(string $line) :int
+{
+    $arrayError = array("?", "!", "#");
+    foreach ($arrayError as $error) {
+        $findError = strpos($line, $error);
+        if ($findError !== false) {
+            echo "error ";
+        }
+}
+    return eval("return (int)$line;");
+}
+echo calculator('4/1+2*3');
---
+++ lw0/lab1.php
@@ -0,0 +1,29 @@
+<?php
+function sumTime(string $timeOne, string $timeTwo) :string
+{
+    $timeOneArray = explode(":", $timeOne);
+    $timeTwoArray = explode(":", $timeTwo);
+
+    $sumArray = array();
+    for ($i = 0; $i < 3; $i++) {
+        $sumArray[] = $timeOneArray[$i] + $timeTwoArray[$i];
+    }
+
+    if ($sumArray[2] >= 60) {
+        $sumArray[1] = $sumArray[1] + 1;
+        $sumArray[2] = $sumArray[2] - 60;
+    }
+
+    if ($sumArray[1] >= 60) {
+        $sumArray[0] = $sumArray[0] + 1;
+        $sumArray[1] = $sumArray[1] - 60;
+    }
+
+    while ($sumArray[0] >= 24) {
+        $sumArray[0] = $sumArray[0] - 24;
+    }
+
+    return $sumArray[0] . ":" . $sumArray[1] . ":" . $sumArray[2];
+}
+
+echo sumTime('10:20:30', '10:20:30');
